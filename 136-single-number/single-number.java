4b3a28b7983f1f38147e41875d52faa8c70cd7d5
Julia Jomon <juliajomon12@gmail.com>
2025-06-16 18:00:31 +0530

    Time: 7 ms (29.42%) | Memory: 46.5 MB (17.63%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int j = 1;
+        if(nums.length == 1){
+            return nums[0];
+        }
+        for(int i = 0 ; i < nums.length -1 ; i= i+2){
+            if(nums[i] != nums[i+1]){
+                return nums[i];
+            }
+            
+        }
+        return nums[nums.length -1];
+    }
+}
