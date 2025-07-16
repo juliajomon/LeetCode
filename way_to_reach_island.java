3b91dc36d2eab4eb318fcf0c05197a806169e7a8
Julia Jomon <juliajomon12@gmail.com>
2025-07-16 22:22:12 +0530

    Update way_to_reach_island.java

--- way_to_reach_island.java
+++ way_to_reach_island.java
@@ -1,3 +1,7 @@
+// You are on island 1, and you want to reach island N.
+// You can go from island i to island j only if i divides j (i.e., j % i == 0).
+
+// You must count how many distinct ways there are to reach island N from island 1 by following this rule.
 import java.util.Scanner;
 
 public class Main {
